793b6d96c424677cca3fade0dd466a937207f567
algoruin <algoruin@outlook.com>
2023-08-14 18:38:51 +0200

    added camera movement code to main.js

--- main.js
+++ main.js
@@ -1,32 +1,52 @@
 const cards = [];
 const scene = document.querySelector(".scene");
-const board = document.querySelector(".board");
+const keys = {};
+const board = {
+  el: document.querySelector(".board"),
+  x: 0,
+  y: 0,
+  xs: 0,
+  ys: 0,
+};
 let pt = 0;
 
 for (var i = 0; i < 10; i++) {
   cards[i] = {
-    el:board.appendChild(document.createElement("div")),
-    x:i*200,
-    y:0
-  }
-  cards[i].el.classList.add('card');
+    el: board.el.appendChild(document.createElement("div")),
+    x: i * 200,
+    y: 0,
+  };
+  cards[i].el.classList.add("card");
 }
 
 const render = (t) => {
   t *= 0.001;
-  
+
   const d = t - pt;
-  
+
   pt = t;
   updateBoard(d);
   window.requestAnimationFrame(render);
-}
+};
 
 const updateBoard = (d) => {
-  for( const c of cards) {
-    c.x += 10*d;
-    c.el.style.transform = `translate(${c.x}px, ${c.y}px)`;
-  }
-}
+  if(keys["ArrowLeft"]) board.x += 100*d;
+  if(keys["ArrowRight"]) board.x -= 100*d;
+  if(keys["ArrowUp"]) board.y += 100*d;
+  if(keys["ArrowDown"]) board.y -= 100*d;
+  translate(board);
+  for (const c of cards) translate(c);
+};
 
-window.requestAnimationFrame(render);
+const translate = (go) => {
+  go.el.style.transform = `translate(${go.x}px, ${go.y}px)`;
+};
+
+addEventListener("keydown", (e) => {
+  keys[e.code] = true;
+});
+
+addEventListener("keyup", (e) => {
+  if (keys[e.code]) keys[e.code] = false;
+});
+requestAnimationFrame(render);
