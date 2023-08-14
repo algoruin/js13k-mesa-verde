1b2529b707b03d5359b9d08151259a8e146e3cbb
algoruin <algoruin@outlook.com>
2023-08-14 17:30:41 +0200

    starter code

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-import './style.css'
+addEventListener("mousemove", (e) => {
+  const mouse = {
+    x: (e.clientX / innerWidth - 0.5) * 2,
+    y: (e.clientY / innerHeight - 0.5) * 2
+  };
+  document.querySelector(".scene").style.transform = `perspective(1000px) rotateX(60deg) rotateZ(${mouse.x * 10}deg) rotateX(${mouse.y * 10}deg)`
+});
