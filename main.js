901c537c60a64eaf5dd316658b94b08585dcab73
algoruin <algoruin@outlook.com>
2023-08-14 18:20:12 +0200

    testing code for game loop

--- main.js
+++ main.js
@@ -1,7 +1,32 @@
-addEventListener("mousemove", (e) => {
-  const mouse = {
-    x: (e.clientX / innerWidth - 0.5) * 2,
-    y: (e.clientY / innerHeight - 0.5) * 2
-  };
-  document.querySelector(".scene").style.transform = `perspective(1000px) rotateX(60deg) rotateZ(${mouse.x * 10}deg) rotateX(${mouse.y * 10}deg)`
-});
+const cards = [];
+const scene = document.querySelector(".scene");
+const board = document.querySelector(".board");
+let pt = 0;
+
+for (var i = 0; i < 10; i++) {
+  cards[i] = {
+    el:board.appendChild(document.createElement("div")),
+    x:i*200,
+    y:0
+  }
+  cards[i].el.classList.add('card');
+}
+
+const render = (t) => {
+  t *= 0.001;
+  
+  const d = t - pt;
+  
+  pt = t;
+  updateBoard(d);
+  window.requestAnimationFrame(render);
+}
+
+const updateBoard = (d) => {
+  for( const c of cards) {
+    c.x += 10*d;
+    c.el.style.transform = `translate(${c.x}px, ${c.y}px)`;
+  }
+}
+
+window.requestAnimationFrame(render);
